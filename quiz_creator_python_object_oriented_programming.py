7c59f46355524e3bfd1cad808fa6cb0145f285e8
IsangBaket? <max.bias@myyahoo.com>
2025-05-30 01:23:59 +0800

    copied previous project to be transformed to functional programming to object oriented programming

---
+++ quiz_creator_python_object_oriented_programming.py
@@ -0,0 +1,35 @@
+# For Assignment 9: Quiz Creator
+# Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+# Write the collected data to a text file. Ask another question until the user chose to exit.
+
+print("-----WELCOME TO MY QUIZ CREATOR-----")
+
+while True:     # asks for user file name
+    txt_file = input("file name with '.txt': ")
+    if txt_file.endswith('.txt'):   # ensures that user puts file extension
+        break
+    print("please add a '.txt' extension!")
+
+while True:
+    question = input("Enter Question(enter e to exit): ")   # asks user for question
+    
+    if question == 'e':   # ends program 
+        break
+
+    print("Input answers for A to D") 
+
+    choices = []
+    for items in ['A', 'B', 'C', 'D']:  # asks user for input for choices A to D
+        answers = input(f"Choices {items}: ")
+        choices.append(answers)
+
+    correct_answer = input("letter of correct answer: ").upper()    # asks user for the correct answer
+
+    with open(txt_file, "a") as file:   # saves input to external text file
+        file.write(f"Question: {question}\n")
+        file.write(f"A: {choices[0]}\n")
+        file.write(f"B: {choices[1]}\n")
+        file.write(f"C: {choices[2]}\n")
+        file.write(f"D: {choices[3]}\n")
+        file.write(f"Correct Answer: {correct_answer}\n")
+        file.write('------\n\n')
