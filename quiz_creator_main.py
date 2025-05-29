ef3ce6187e6cb71d06482b2f112cec34f8cf1435
IsangBaket? <max.bias@myyahoo.com>
2025-05-30 02:03:12 +0800

    added main module

---
+++ quiz_creator_main.py
@@ -0,0 +1,8 @@
+from quiz_creator_python_object_oriented_programming import QuizCreator
+
+def main():
+    quiz_reader = QuizCreator()
+    quiz_reader.quiz_creator()
+
+if __name__ == "__main__":
+    main()
