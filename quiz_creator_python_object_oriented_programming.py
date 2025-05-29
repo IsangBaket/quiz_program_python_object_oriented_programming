f3122efcbdbd0543a681567b0e3c5a2fbb6b148d
IsangBaket? <max.bias@myyahoo.com>
2025-05-30 01:37:10 +0800

    transformed code from procedural to functional

--- quiz_creator_python_object_oriented_programming.py
+++ quiz_creator_python_object_oriented_programming.py
@@ -4,32 +4,40 @@
 
 print("-----WELCOME TO MY QUIZ CREATOR-----")
 
-while True:     # asks for user file name
-    txt_file = input("file name with '.txt': ")
-    if txt_file.endswith('.txt'):   # ensures that user puts file extension
-        break
-    print("please add a '.txt' extension!")
+def file_name():
+    while True:     # asks for user file name
+        txt_file = input("file name with '.txt': ")
+        if txt_file.endswith('.txt'):   # ensures that user puts file extension
+            break
+        print("please add a '.txt' extension!")
 
-while True:
-    question = input("Enter Question(enter e to exit): ")   # asks user for question
-    
-    if question == 'e':   # ends program 
-        break
+def quiz_creator():
+    while True:
+        question = input("Enter Question(enter e to exit): ")   # asks user for question
+        
+        if question == 'e':   # ends program 
+            break
 
-    print("Input answers for A to D") 
+        print("Input answers for A to D") 
 
-    choices = []
-    for items in ['A', 'B', 'C', 'D']:  # asks user for input for choices A to D
-        answers = input(f"Choices {items}: ")
-        choices.append(answers)
+        choices = []
+        for items in ['A', 'B', 'C', 'D']:  # asks user for input for choices A to D
+            answers = input(f"Choices {items}: ")
+            choices.append(answers)
 
-    correct_answer = input("letter of correct answer: ").upper()    # asks user for the correct answer
+        correct_answer = input("letter of correct answer: ").upper()    # asks user for the correct answer
 
-    with open(txt_file, "a") as file:   # saves input to external text file
-        file.write(f"Question: {question}\n")
-        file.write(f"A: {choices[0]}\n")
-        file.write(f"B: {choices[1]}\n")
-        file.write(f"C: {choices[2]}\n")
-        file.write(f"D: {choices[3]}\n")
-        file.write(f"Correct Answer: {correct_answer}\n")
-        file.write('------\n\n')
+        with open(txt_file, "a") as file:   # saves input to external text file
+            file.write(f"Question: {question}\n")
+            file.write(f"A: {choices[0]}\n")
+            file.write(f"B: {choices[1]}\n")
+            file.write(f"C: {choices[2]}\n")
+            file.write(f"D: {choices[3]}\n")
+            file.write(f"Correct Answer: {correct_answer}\n")
+            file.write('------\n\n')
+
+def main():
+    filename = file_name()
+    quiz_creator(filename)
+
+main()
